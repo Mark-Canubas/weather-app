904ecd7ec9b083ffd34b431c2d0b77c22409243d
Mark-Canubas <Canubasmarkroland@gmail.com>
2025-11-17 09:29:18 +0800

    refactor: update routing to simplify navigation and redirect to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
+        path: '**',
         loadComponent: () => import('./pages/home/home').then(c => c.Home)
     }
 ];
