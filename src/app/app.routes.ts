4e88f9101bcaa26f35f7bf489fff180f29fcc749
Mark-Canubas <Canubasmarkroland@gmail.com>
2025-11-17 09:34:35 +0800

    refactor: fix routing to ensure proper redirection to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '**',
+        redirectTo: '',
+    },
+    {
+        path: '',
         loadComponent: () => import('./pages/home/home').then(c => c.Home)
     }
 ];
